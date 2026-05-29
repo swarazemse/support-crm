a6dc478a9ff261ae2306b9baf686cd43f724b6d8
Swara Zemse <ashwinishahasane1479@gmail.com>
2026-05-30 03:19:09 +0530

    resolve issue with deleting ticket generated recently and adding it to ticket history

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API = axios.create({
   
- //baseURL: "https://support-crn-backend-production.up.railway.app/api",
-  baseURL: "http://127.0.0.1:8000/api",
+ baseURL: "https://support-crn-backend-production.up.railway.app/api",
+  //baseURL: "http://127.0.0.1:8000/api",
   timeout: 15000
 });
 
