9a4380284b8ee14d6512e6a1324466d68ae3c712
Swara Zemse <ashwinishahasane1479@gmail.com>
2026-05-29 06:34:06 +0530

    Connect frontend to Railway backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api"
+  baseURL: "https://support-crn-backend-production.up.railway.app/api"
 });
 
 export default API;
